5a8486f2459c89818bc886c97b022de5b5ac131d
joshuaellis555 <joshuaellis101@gmail.com>
2018-08-18 02:45:18 -0500

    old unpushed resource types update

--- source/resources/ResourceTypes.hx
+++ source/resources/ResourceTypes.hx
@@ -12,11 +12,11 @@ class ResourceTypes
 	public static var Productivity(default, never):FlxColor = FlxColor.BLUE;
 	public static var Science(default, never):FlxColor = FlxColor.WHITE;
 	public static var Credits(default, never):FlxColor = FlxColor.ORANGE;
-	public static var Materials(default, never):FlxColor = FlxColor.CYAN;
+	public static var Materials(default, never):FlxColor = FlxColor.GRAY;
 	
 	//  \/ testing \/ 
 	public static var Power(default, never):FlxColor = FlxColor.RED;
-	public static var Culture(default, never):FlxColor = FlxColor.GRAY;
+	public static var Culture(default, never):FlxColor = FlxColor.CYAN;
 	public static var Influence(default, never):FlxColor = FlxColor.PURPLE;
 	
 	public static var types(default, never):Array<FlxColor> = [ResourceTypes.Organic, ResourceTypes.Productivity, ResourceTypes.Science, ResourceTypes.Credits, ResourceTypes.Materials,
